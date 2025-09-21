0954b7e9ea0009be6e944f5d13a192709c471d6a
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-09-21 16:48:10 -0300

    primeiro commit

---
+++ src/components/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input placeholder='Digite o seu nome' />
+        </div>
+    );
+}
+
+export default CampoTexto;
