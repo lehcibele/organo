933c1b95abfcdc94e11066f389bf609a24fbaf6d
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-09-24 10:34:23 -0300

    continuando o projeto

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,12 +1,26 @@
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+
+    const aoDigitado = (evento) => {
+        // target -> evento do js
+        // pega o que p usuario digitar no input
+        // setValor(evento.target.value)
+        // console.log(valor);
+
+        props.aoAlterado(evento.target.value);
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder={props.placeholder} />
+
+            {/* required deixa o campo obrigatorio se obrigatorio for true, isso vem do arquivo index.js (Formulario) */}
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
+
+            {/* onChange quando o input for alterado pelo usuario vai ser executado uma função */}
         </div>
     );
 }
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,15 +1,72 @@
+import { useState } from 'react';
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ];
+
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [imagem, setImagem] = useState('');
+    const [time, setTime] = useState('')
+
+
+
+    // Submisão do formulario
+    const aoSalvar = (evento) => {
+        evento.preventDefault() // previne o evento padrão de recarregar a página
+        console.log('Form foi submetido => ', nome, cargo, imagem, time);
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
-                <CampoTexto label="Time" placeholder="Digite o seu time"/>
+
+                {/* obrigatorio = true é para deixar o campo sendo obrigatorio, no arquivo CampoTexto tem o required */}
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Nome" 
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Cargo" 
+                    placeholder="Digite o seu cargo"
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+
+                <CampoTexto 
+                    label="Imagem" 
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+
+                <ListaSuspensa 
+                    obrigatorio={true} 
+                    label="Time" 
+                    itens={times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}
+                />
+
+                <Botao texto="Criar Card" />
             </form>
         </section>
     )
